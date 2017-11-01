f2a0505c201cef02884a529a15b01dc0e38cb689
“Richard Middleton” <Richard@richardmiddleton.me>
2017-11-01 15:03:37 +0000

    babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets:['es2015']
+                }
+            }
+        ]
     }
 }
