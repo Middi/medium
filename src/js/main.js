d9686d790e8e4e5629684b2c7a27ef547850f1e7
“Richard Middleton” <Richard@richardmiddleton.me>
2017-11-06 19:07:32 +0000

    comment box javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,29 @@
-(function($) { // Begin jQuery
-    $(function() { // DOM ready
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      // Hamburger to X toggle
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-    }); // end DOM ready
-  })(jQuery); // end jQuery
+$(function () {
+
+  var write = $('.write-response');
+  var user = $('.response-user');
+
+  function complete() {
+    user.fadeIn(300);
+  };
+
+  function completeOut() {
+    write.fadeIn(300);
+  };
+
+  $('#write-comment').click(function (e) {
+
+    // Used to stop the event bubbling..
+    e.stopPropagation()
+    $('.text-area-div').slideDown('slow');
+    write.fadeOut(200, complete);
+    $('.text-area').focus();
+  });
+
+  // Hide the "text-area-div" by clicking outside container
+  $(document).click(function () {
+    $('.text-area-div').slideUp('slow');
+    user.fadeOut(200, completeOut);
+  });
+
+});
