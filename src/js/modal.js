d3a4c2804248751527f65d7efb1c0735b91cbe84
“Richard Middleton” <Richard@richardmiddleton.me>
2017-11-07 00:46:57 +0000

    tidied modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,23 +5,18 @@ $(function () {
     });
 
     // Open modal
-    $('#mimg').click(function (e) {
-        // Used to stop the event bubbling   
+    $('#mimg').click(function (e) { 
         e.stopImmediatePropagation();
-        // change cursor
         $('img').css('cursor', 'zoom-in');
         $('#modal').fadeOut(400);
     });
 
-    // Used to stop the event bubbling
     $('img').on('click', function (e) {
-        // Used to stop the event bubbling  
         e.stopPropagation();
         // get src of image
         var src = $(this).attr('src');
         // set src for image
         $('#mimg').attr('src', src);
-        // change cursor
         $('img').css('cursor', 'zoom-out');
         $('#modal').fadeIn(400);
     });
